b6ed0838969ed1440e491df3272af356bab3e8b7
wwolny <wwolny1@gmail.com>
2022-01-08 00:09:30 +0100

    add lang msg

--- do_celu/utils/utils.py
+++ do_celu/utils/utils.py
@@ -10,7 +10,8 @@ from do_celu.config import Config
 
 def create_message(config_file: Config, send_to: str, performative: str, msg_body: str) -> Message:
   msg = Message(to=send_to)
-  msg.set_metadata('performative', performative)
-  msg.set_metadata('ontology', config_file.ONTOLOGY)
+  msg.set_metadata("performative", performative)
+  msg.set_metadata("ontology", config_file.ONTOLOGY)
+  msg.set_metadata("langauge", "JSON")
   msg.body = msg_body
   return msg
